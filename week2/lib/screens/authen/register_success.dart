6c9b63990aef8f9aabe1600395f0faf5d1fd4d6a
tranvanhoanghai <74179524+tranvanhoanghai@users.noreply.github.com>
2021-11-17 13:29:14 +0700

    fix

--- week2/lib/screens/authen/register_success.dart
+++ week2/lib/screens/authen/register_success.dart
@@ -8,49 +8,54 @@ class RegisterSuccess extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: introduceBg,
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Column(
-                    children: const [
-                      Image(
-                        image: AssetImage('assets/images/auths/reg_done.png'),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            MaterialButton(
-              height: 58,
-              minWidth: 340,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const Login(),
-                  ),
-                );
-              },
-              child: const Text(
-                'Login Now',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 28.0, vertical: 16.0),
+          width: double.infinity,
+          color: introduceBg,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: const [
+                        Image(
+                          image: AssetImage('assets/images/auths/reg_done.png'),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              color: const Color(0xFFF7CA18),
-            ),
-          ],
-        ));
+              MaterialButton(
+                height: 58,
+                minWidth: 340,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Login(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Login Now',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
+                color: const Color(0xFFF7CA18),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
-
